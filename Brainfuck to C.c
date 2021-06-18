7877ef16e83855f4ed065f7b4f441f1d637c81d9
0Nera <71536443+0Nera@users.noreply.github.com>
2021-06-18 11:03:30 +0300

    Create simple translator from Brainfuck to C

---
+++ Brainfuck to C.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define MAX_LEN 1500000
+#include <string.h>
+
+int main() {
+    char code[]="+++++++++++++++++++++++++++++++++++++++++++++ +++++++++++++++++++++++++++.+++++++++++++++++ ++++++++++++.+++++++..+++.------------------- --------------------------------------------- ---------------.+++++++++++++++++++++++++++++ ++++++++++++++++++++++++++.++++++++++++++++++ ++++++.+++.------.--------.------------------ --------------------------------------------- ----.-----------------------.";
+    int i = 0;
+    int pars = 0;
+    int size =sizeof(code) / sizeof(char);
+    char arr[30000];
+    char *output_file="int i = 0; char arr[30000]; memset(arr, 0, sizeof(arr));";
+    memset(arr, 0, sizeof(arr)); 
+    printf("Size of code = %d\n", size);
+    while (
+            pars!=size
+            ){
+        switch(code[pars]){
+            case '>': strncat(output_file,"i++;",MAX_LEN); pars++;
+            case '<': strncat(output_file,"i--;",MAX_LEN);pars++;
+            case '+': strncat(output_file,"arr[i]++;",MAX_LEN);pars++;
+            case '-': strncat(output_file,"arr[i]--;",MAX_LEN);pars++;
+            case '.': strncat(output_file,"putchar(arr[i]);",MAX_LEN);pars++;
+            case ',': strncat(output_file,"arr[i] = getchar();",MAX_LEN)pars++;
+            case '[': strncat(output_file,"while(arr[i]){",MAX_LEN)
+            case ']': strncat(output_file,"}",MAX_LEN) 
+            default: pars++;
+
+        }
+
+    }
+    FILE *fin;
+    fin = fopen("output.c", "r");
+    fprintf (fin,output_file);
+    fclose(fin);
+    return pars;
+
+}
